0a140af284d8509fd77695ff87879057294e0988
lukaval <forumchanko@gmail.com>
2024-06-06 15:15:49 +0300

    fix check style

--- src/test/java/ru/practicum/shareit/item/ItemServiceTest.java
+++ src/test/java/ru/practicum/shareit/item/ItemServiceTest.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.booking.model.BookingStatus;
 import ru.practicum.shareit.booking.storage.BookingRepository;
